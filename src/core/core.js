4dabbe5b0b2dcc2609f719f50018f1cfa0d9e368
Rodolfo <rodolfoviolac@gmail.com>
2021-03-21 17:23:31 -0300

    fix interval name

--- src/core/core.js
+++ src/core/core.js
@@ -8,10 +8,10 @@ async function startCore() {
     Object.entries(hash.table).forEach(([key, val]) => {
         if (val.type === 'object') {
             setTimeout(() => { v.getValue(val.oid) }, Math.floor((Math.random() * 200) + 1));
-            hash.table[key].interaval = setInterval((async () => {v.getValue(val.oid)}), val.runInterval);
+            hash.table[key].interval = setInterval((async () => {v.getValue(val.oid)}), val.runInterval);
         } else if (val.type === 'table') {
             setTimeout(() => { t.getTable(val.oid) }, Math.floor((Math.random() * 500) + 1));
-            hash.table[key].interaval = setInterval((async () => {t.getTable(val.oid)}), val.runInterval);
+            hash.table[key].interval = setInterval((async () => {t.getTable(val.oid)}), val.runInterval);
         }
     });
 }
